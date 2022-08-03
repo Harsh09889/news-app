f312d682b393a98407fea61583579ed69ad4026b
harsh <harsh09889@gmail.com>
2022-08-03 21:01:03 +0530

    deploying build

--- src/components/News.js
+++ src/components/News.js
@@ -8,47 +8,48 @@ import PropTypes from 'prop-types'
 export class News extends Component {
 
   static defaultProps = {
-    country : 'in',
+    country: 'in',
     pageSize: 8,
-    category:'general'
+    category: 'general'
   }
 
   static propTypes = {
-    country:PropTypes.string,
-    pageSize:PropTypes.number,
-    category:PropTypes.string,  
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
   }
 
   constructor() {
     super();
     // console.log("hello world")
     this.state = {
-      articles:[],
+      articles: [],
       loading: false,
       page: 1,
     };
   }
 
-   updateData = async()=>{
+  updateData = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=23d9cd8ac58a4cf4bc69b674c27caa27&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({
-      loading:true
-    })  
+      loading: true
+    })
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles, 
-      totalResults:parsedData.totalResults,
-      loading:false
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false
     });
   }
 
   handleNext = async () => {
-    this.setState({page:this.state.page+1})
+    this.setState({ page: this.state.page + 1 })
     this.updateData()
   };
 
   handlePrev = async () => {
-    this.setState({page:this.state.page-1})
+    this.setState({ page: this.state.page - 1 })
     this.updateData()
   };
 
@@ -59,41 +60,41 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-5">
-        
-        <h2 style={{ textAlign: "center" }}>Aaj ka samachar for meri MUMMY JI</h2>
+
+        <h2 style={{ textAlign: "center" }}>Today's Headlines</h2>
         {
           this.state.loading && <Spinner />
-        } 
+        }
         <div className="row my-4 mx-auto">
           {
             !this.state.loading &&
-          
-          (this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4  " key={element.url}>
-                <NewsItem
-                  title={element.title}
-                  description={element.description}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          }))
-          
-          
+
+            (this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4  " key={element.url}>
+                  <NewsItem
+                    title={element.title}
+                    description={element.description}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            }))
+
+
           }
         </div>
         <div className="buttonContainer d-flex justify-content-between">
-          <button className="btn btn-dark"  
-          onClick={this.handlePrev}
-          disabled={this.state.page<=1}
+          <button className="btn btn-dark"
+            onClick={this.handlePrev}
+            disabled={this.state.page <= 1}
           >
             Previous
           </button>
           <button
             className="btn btn-dark"
-            disabled={ this.state.page + 1 > Math.ceil(this.state.totalResults / this.pageSize)}
+            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.pageSize)}
             onClick={this.handleNext}
           >
             Next
